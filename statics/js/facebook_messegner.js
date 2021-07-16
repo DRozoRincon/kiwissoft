aa72d250bf28a085eaa45e767425974ff7f934a0
David rozo <drozorincon@gmail.com>
2021-07-15 22:24:20 -0500

    Integracion chat facebook messenger

---
+++ statics/js/facebook_messegner.js
@@ -0,0 +1,21 @@
+'use strict'
+window.addEventListener('load', ()=>{
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "107821434853051");
+      chatbox.setAttribute("attribution", "biz_inbox");
+
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v11.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+});
