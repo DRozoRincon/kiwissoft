c5aa10211c2b29a35a1b67d14c385bce035b08c2
David rozo <drozorincon@gmail.com>
2021-07-19 21:30:07 -0500

    testimonios hechos

--- statics/js/glider_slider.js
+++ statics/js/glider_slider.js
@@ -1,6 +1,6 @@
 'use strict'
 window.addEventListener('load', ()=>{
-    new Glider(document.querySelector('#testimoniosGlider'), {
+    const glider = new Glider(document.querySelector('#testimoniosGlider'), {
         slidesToShow: 1,
         slidesToScroll: 1,
         scrollLock: true,
@@ -8,4 +8,31 @@ window.addEventListener('load', ()=>{
         dots: '#puntos',
         draggable: true
     });
+
+    function sliderAuto(slider, miliseconds) {
+        const slidesCount = slider.track.childElementCount;
+        let slideTimeout = null;
+        let nextIndex = 1;
+       
+        function slide () {
+          slideTimeout = setTimeout(
+            function () {
+              if (nextIndex >= slidesCount ) {
+                nextIndex = 0;
+              }
+              slider.scrollItem(nextIndex++);
+            },
+            miliseconds
+          );
+        }
+       
+        slider.ele.addEventListener('glider-animated', function() {
+          window.clearInterval(slideTimeout);
+          slide();
+        });
+       
+        slide();
+    }
+       
+    sliderAuto(glider, 4000)
 });
